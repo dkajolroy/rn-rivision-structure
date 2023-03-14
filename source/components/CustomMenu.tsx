ef7c95b86733a44a006e6a68da1263a9b4c0b996
kajolroy <shrikajol@gmail.com>
2023-03-15 00:06:56 +0600

    add screen

---
+++ source/components/CustomMenu.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function CustomMenu() {
+  return (
+    <View>
+      <Text>CustomMenu</Text>
+    </View>
+  );
+}
